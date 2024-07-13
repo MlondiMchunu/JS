0c3a4c3fef87c3ea8227e902cebbd6e7154d8b20
Stopa <sikhuliso@gmail.com>
2024-07-13 13:03:11 +0200

    functionObject

---
+++ Basics/functionObject.js
@@ -0,0 +1,9 @@
+<html>
+    <head></head>
+    <body>
+        <script>
+            var calc = Function("num1","num2","return num1 + num2");
+            document.writeln(calc(5,3));
+        </script>
+    </body>
+</html>
