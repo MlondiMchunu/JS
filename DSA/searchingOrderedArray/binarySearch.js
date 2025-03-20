33332d55304f29e153ac1f378230cafad08b4ab7
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-03-20 18:16:43 +0200

    refactor code

--- DSA/searchingOrderedArray/binarySearch.js
+++ DSA/searchingOrderedArray/binarySearch.js
@@ -3,18 +3,17 @@ function binarySearch(array, searchValue) {
     let upperBound = array.length - 1;
 
     while (lowerBound <= upperBound) {
-        const midpoint = Math.floor((upperBound + lowerBound) / 2);
-        const valueAtMidpoint = array[midpoint];
+        const midpoint = Math.floor((lowerBound + upperBound) / 2);
+        const midPointValue = array[midpoint];
 
-        if (searchValue === valueAtMidpoint) {
+        if (midPointValue === searchValue) {
             return midpoint;
-        } else if (searchValue < valueAtMidpoint) {
+        } else if (midPointValue > searchValue) {
             upperBound = midpoint - 1;
-        } else if (searchValue > valueAtMidpoint) {
+        } else if (midPointValue < searchValue) {
             lowerBound = midpoint + 1;
         }
-    }
-    return null
+    }return null;
 }
 
 console.log(binarySearch([6, 23, 43, 65, 98, 234, 654, 876, 923, 940, 1000, 1943], 654))
