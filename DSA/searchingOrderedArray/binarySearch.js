bffaf0010397976f00f7f084c98aa6baef81bd23
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-03-20 12:55:18 +0200

    commit binarySearch

---
+++ DSA/searchingOrderedArray/binarySearch.js
@@ -0,0 +1,18 @@
+function binarySearch(array, searchValue) {
+    let lowerBound = 0;
+    let upperBound = array.length - 1;
+
+    while (lowerBound <= upperBound) {
+        const midpoint = Math.floor((upperBound + lowerBound) / 2);
+        const valueAtMidpoint = array[midpoint];
+
+        if (searchValue === valueAtMidPoint) {
+            return midpoint;
+        }else if(searchValue < valueAtMidpoint){
+            upperBound = midpoint -1;
+        }else if(searchValue > valueAtMidpoint){
+            lowerBound = midpoint + 1;
+        }
+    }
+    return null
+}
