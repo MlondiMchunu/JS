a99c90f6d749203c1ea96611613f7b4971a55de0
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-03-20 13:01:42 +0200

    correct valueAtMidpoint variable and call function

--- DSA/searchingOrderedArray/binarySearch.js
+++ DSA/searchingOrderedArray/binarySearch.js
@@ -6,13 +6,15 @@ function binarySearch(array, searchValue) {
         const midpoint = Math.floor((upperBound + lowerBound) / 2);
         const valueAtMidpoint = array[midpoint];
 
-        if (searchValue === valueAtMidPoint) {
+        if (searchValue === valueAtMidpoint) {
             return midpoint;
-        }else if(searchValue < valueAtMidpoint){
-            upperBound = midpoint -1;
-        }else if(searchValue > valueAtMidpoint){
+        } else if (searchValue < valueAtMidpoint) {
+            upperBound = midpoint - 1;
+        } else if (searchValue > valueAtMidpoint) {
             lowerBound = midpoint + 1;
         }
     }
     return null
 }
+
+console.log(binarySearch([6, 23, 43, 65, 98, 234, 654, 876, 923, 940, 1000, 1943], 654))
