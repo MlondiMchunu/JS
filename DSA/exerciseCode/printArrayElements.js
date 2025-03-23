ec4fb4e2dcf6b303038282df49778f47786d8df9
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-03-23 18:02:56 +0200

    latest commit

---
+++ DSA/exerciseCode/printArrayElements.js
@@ -0,0 +1,6 @@
+let things = ['apples', 'baboons', 'cribs', 'vulcimers'];
+
+console.log("Index", "\t", "Elements")
+for (const [index, elements] of things.entries()) {
+    console.log(index, "\t", elements, " ")
+}
